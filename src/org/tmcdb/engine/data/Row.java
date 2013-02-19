c639a600cfa764ded125f7f86fcbbdea28c72015
Pavel Talanov <talanov.pavel@gmail.com>
2013-02-19 15:16:32 +0400

    Remove dead code

--- src/org/tmcdb/engine/data/Row.java
+++ src/org/tmcdb/engine/data/Row.java
@@ -35,34 +35,4 @@ public class Row {
     public Collection<Column> getColumns() {
         return columnToValue.keySet();
     }
-    //TODO:
-//    public int sizeInBytes() {
-//        int size = 0;
-//        for (int i = 0; i < columns.size(); ++i) {
-//            if (columns.get(i).getType() == NumericType.INT) {
-//                return Integer.SIZE / Byte.SIZE;
-//            } else if (columns.get(i).getType() == NumericType.DOUBLE) {
-//                return Double.SIZE / Byte.SIZE;
-//            } else {
-//                try {
-//                    return ((String) values.get(i)).getBytes("UTF-8").length + Short.SIZE / Byte.SIZE;
-//                } catch (UnsupportedEncodingException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//
-//        return size;
-//    }
-//
-//    public String getStringRepresentationAt(int index) {
-//        Type type = columns.get(index).getType();
-//        Object value = values.get(index);
-//
-//        if (type == NumericType.INT) {
-//            return String.format("%d", (Integer) value);
-//        } else if (type == NumericType.DOUBLE) {
-//            return String.format("%f", (Double) value);
-//        } else return (String) value;
-//    }
 }
